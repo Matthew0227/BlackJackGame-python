a39c6e638ade048ffe5b5aa7a4f50ce487ba8838
Matthew0227 <matthewjaveniar2@gmail.com>
2024-09-30 22:59:37 +0800

    done adding the deck and variable player and dealers hand

--- blackjackgame.py
+++ blackjackgame.py
@@ -1,4 +1,15 @@
 # deck of cards /  player dealer hand
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10,
+        2, 3, 4, 5, 6, 7, 8, 9, 10,
+        2, 3, 4, 5, 6, 7, 8, 9, 10,
+        2, 3, 4, 5, 6, 7, 8, 9, 10,
+        'A', 'J', 'Q', 'K',
+        'A', 'J', 'Q', 'K',
+        'A', 'J', 'Q', 'K',
+        'A', 'J', 'Q', 'K',]
+
+player_hand = []
+dealer_hand = []
 # deal the cards
 # calculate the total of each hand
 # check for winner
