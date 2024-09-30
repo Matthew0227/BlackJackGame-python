d78caafb73851c40f409fb0d54369298ae42fde3
Matthew0227 <matthewjaveniar2@gmail.com>
2024-09-30 23:06:27 +0800

    done adding the deal card function and tested it

--- blackjackgame.py
+++ blackjackgame.py
@@ -1,3 +1,5 @@
+import random
+
 # deck of cards /  player dealer hand
 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10,
         2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -11,6 +13,13 @@ deck = [2, 3, 4, 5, 6, 7, 8, 9, 10,
 player_hand = []
 dealer_hand = []
 # deal the cards
+def deal_card(turn):
+    card = random.choice(deck)
+    turn.append(card)
+    deck.remove(card)
+
+deal_card(player_hand)
+print(player_hand)
 # calculate the total of each hand
 # check for winner
 # game loop
